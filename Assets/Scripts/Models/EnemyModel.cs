a560c8d00996781229255745d86b377e159c9dc5
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2019-12-15 16:42:46 +0300

    Add EnemyModel

---
+++ Assets/Scripts/Models/EnemyModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class EnemyModel
+{
+    public float MovementSpeed;
+    public float RangeOfMovement;
+    private TimeSpan ImmobilityTime;
+    public int HealthPoints;
+    public int RateOfFire;
+    public int DamagePerShot;
+}
