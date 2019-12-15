7a4f305e30a34dc43cf26dccf2291ef0e8d0132a
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2019-12-15 20:00:31 +0300

    Add enemy rotation depend on player position

---
+++ Assets/Scripts/Components/ObjectRotationComponent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Views;
+
+namespace Components
+{
+    public class ObjectRotationComponent : MonoBehaviour
+    {
+        private Transform _target;
+
+        private float _speed;
+
+        private void Awake()
+        {
+            _target = FindObjectOfType<PlayerView>().transform;
+
+            _speed = 10f;
+        }
+
+        private void Update()
+        {
+            var direction = _target.position - this.gameObject.transform.position;
+            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+            var rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            this.gameObject.transform.rotation = Quaternion.Slerp(
+                this.gameObject.transform.rotation,
+                rotation,
+                _speed * Time.deltaTime);
+        }
+    }
+}
