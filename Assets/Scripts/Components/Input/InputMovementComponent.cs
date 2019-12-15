eb5a92d721a75f0c28f06fc5f5487a8b9ac4e55c
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2019-12-15 17:44:18 +0300

    Add ability to move player via WASD buttons

---
+++ Assets/Scripts/Components/Input/InputMovementComponent.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Components.Input
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class InputMovementComponent : MonoBehaviour
+    {
+        private Rigidbody2D _rigidbody2D;
+
+        private float _movementSpeed;
+
+        private Vector2 _movementVector2;
+
+        private void Awake()
+        {
+            _rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
+
+            _movementSpeed = 10;
+        }
+
+        private void Update()
+        {
+            _movementVector2.x = UnityEngine.Input.GetAxisRaw("Horizontal");
+            _movementVector2.y = UnityEngine.Input.GetAxisRaw("Vertical");
+        }
+
+        private void FixedUpdate()
+        {
+            _rigidbody2D.MovePosition(_rigidbody2D.position + _movementSpeed * Time.fixedDeltaTime * _movementVector2);
+        }
+    }
+}
