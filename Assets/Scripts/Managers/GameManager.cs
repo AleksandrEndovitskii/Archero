059b50f074da522a813314b166bf8165aeeb7280
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2019-12-15 16:08:35 +0300

    Add GameManager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        // static instance of GameManager which allows it to be accessed by any other script 
+        public static GameManager Instance;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+
+                DontDestroyOnLoad(gameObject); // sets this to not be destroyed when reloading scene 
+            }
+            else
+            {
+                if (Instance != this)
+                {
+                    // this enforces our singleton pattern, meaning there can only ever be one instance of a GameManager 
+                    Destroy(gameObject);
+                }
+            }
+
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+
+        }
+    }
+}
