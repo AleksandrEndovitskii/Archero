ac09848f5e25336a8a7a8b7a06b7953a6ccaeb2d
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2019-12-15 17:55:29 +0300

    Add EnemyView to GameObjectManager

--- Assets/Scripts/Managers/GameObjectManager.cs
+++ Assets/Scripts/Managers/GameObjectManager.cs
@@ -9,14 +9,19 @@ namespace Managers
 #pragma warning disable 0649
         [SerializeField]
         private PlayerView playerView;
+        [SerializeField]
+        private EnemyView enemyView;
 #pragma warning restore 0649
 
         [NonSerialized]
         public PlayerView PlayerViewInstance;
+        [NonSerialized]
+        public EnemyView EnemyViewInstance;
 
         public void Initialize()
         {
             PlayerViewInstance = Instantiate(playerView);
+            EnemyViewInstance = Instantiate(enemyView);
         }
     }
 }
